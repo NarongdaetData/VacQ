85e7151a76b002e23f9a309c5ffa0062dc27c08e
DESKTOP-PG91I3J\Windows <6330146421@student.chula.ac.th>
2023-02-19 19:46:59 +0700

    add Authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -10,7 +11,10 @@ const app=express();
 app.use(express.json());
 
 const hospitals = require('./routes/hospitals');
+const auth = require('./routes/auth');
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT,console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
