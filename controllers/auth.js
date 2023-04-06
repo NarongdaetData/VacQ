cf3463cb6c36f659908a6e1a8cb01e584ceae95f
NarongdaetData <6330146421@student.chula.ac.th>
2023-04-06 16:38:53 +0700

    Security & OpenAPI

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,6 +52,8 @@ exports.register = async (req, res, next) => {
 //@route POST /api/v1/auth/login
 //@access Public
 exports.login = async (req, res, next) => {
+
+  try {
   const { email, password } = req.body;
   //Validate email & password
   if (!email || !password) {
@@ -81,6 +83,9 @@ exports.login = async (req, res, next) => {
   //const token=user.getSignedJwtToken() ;
   //res.status(200).json ({success:true,token});
   sendTokenResponse(user, 200, res);
+}catch(err){
+  return res.status(401).json({success:false, msg:'Cannot convert email or password to string'});
+}
 };
 
 //Get token from model, create cookie and send response
@@ -95,5 +100,20 @@ exports.getMe = async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: user,
+  });
+};
+
+//@desc Log user out / clear cookie
+//@route GET /v1/auth/logout
+//@access Private
+
+exports.logout = async (req, res, next) => {
+  res.cookie('token','none',{
+    expires: new Date(Date.now(+10*100)),
+    httpOnly:true
+  })
+  res.status(200).json({
+    success: true,
+    data: {}
   });
 };
--- server.js
+++ server.js
@@ -1,27 +1,69 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser')
-const connectDB = require('./config/db')
-const cors = require('cors')
+const cookieParser = require('cookie-parser');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss=require('xss-clean')
+const rateLimit=require('express-rate-limit')
+const hpp=require('hpp');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express')
+
+
+
+const hospitals = require('./routes/hospitals');
+const appointments = require('./routes/appointments');
+const auth = require('./routes/auth');
+
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
 //COnnect to daatabase
 connectDB();
 const app=express();
+
+const swaggerOption = {
+    swaggerDefinition:{
+        openapi: '3.0.0',
+        info: {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:8080/api/v1'
+            }
+        ],
+    },
+
+    apis:['./routes/*.js'],
+};
+const swaggerDocs=swaggerJsDoc(swaggerOption);
+app.use('/api-docs', swaggerUI.serve , swaggerUI.setup(swaggerDocs) );
+
+
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(rateLimit({
+    windowsMs: 10*60*1000,//10 mins
+    max:100
+}
+));
+app.use(hpp());
 
-const hospitals = require('./routes/hospitals');
-const appointments = require('./routes/appointments');
-const auth = require('./routes/auth');
 
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/appointments',appointments);
 app.use('/api/v1/auth',auth);
 
 
-app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT,console.log('Server running in ', process.env.NODE_ENV, 'mode on port ', PORT));
